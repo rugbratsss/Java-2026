d5b8ff8106535589e9ba18afc1c24b0a4ffc4949
rugbratsss <rugbratsss@gmail.com>
2026-04-24 21:40:32 +0200

    Add user rating input functionality to StreamingServicesApplication

--- StreamingServicesApplication.java
+++ StreamingServicesApplication.java
@@ -1,19 +1,31 @@
 import java.util.Scanner;
 import java.util.Random;
 public class StreamingServicesApplication {
+    public static int populateRatings(int[] userRatings, String[] serviceProviders)
+    {
+        Scanner kb = new Scanner(System.in);
+        for(int i = 0; i < userRatings.length; i++)
+        {
+            System.out.println("Enter user ratings(1-5) for " + serviceProviders[i]);
+            userRatings[i] = kb.nextInt();
+        }
+    }
+    }
     public static void populateSubscribers(int[] subscribers)
     {
         Random rand = new Random();
 
         for(int i = 0; i < subscribers.length; i++){
-            subscribers[i] = r
+            subscribers[i] = rand.nextInt(400001) + 100000;
         }
     }
     public static void main(String[] args){
         String[] serviceProviders = {"YouTube", "Netflix","DSTV", "ShowMax", "PrimeTV", "SABC"};
         String[] originCountry = {"United States", "United States", "South Africa", "South Africa", "United States", "South Africa"}; 
 
-        int[] subscribers = new int (serviceProviders.length);
-        int[] userRatings = new int (serviceProviders.length);
+        int[] subscribers = new int [serviceProviders.length];
+        int[] userRatings = new int [serviceProviders.length];
+        populateSubscribers(subscribers);
+        populateRatings(userRatings,serviceProviders);
     }
 }
